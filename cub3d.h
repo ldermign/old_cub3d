10f5d12071bedd54eeb205c23eb90f9ac20dc68e
ldermign <ldermign@student.42.fr>
2021-03-28 16:04:52 +0200

    ajout minilib

--- cub3d.h
+++ cub3d.h
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldermign <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 15:40:28 by ldermign          #+#    #+#             */
-/*   Updated: 2021/03/28 15:49:14 by ldermign         ###   ########.fr       */
+/*   Updated: 2021/03/28 15:56:45 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
 #endif
