93cb21caea2f5cc5aff402bc240307d5035b884e
ldermign <ldermign@student.42.fr>
2021-04-02 14:49:12 +0200

    ttt

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 15:40:28 by ldermign          #+#    #+#             */
-/*   Updated: 2021/04/01 13:09:58 by ldermign         ###   ########.fr       */
+/*   Updated: 2021/04/02 14:47:02 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,31 @@ typedef struct  s_data {
     int         endian;
 }               t_data;
 
+typedef struct  s_mlx {
+    void        *mlx;
+    void        *win;
+}               t_mlx;
 
+typedef struct  s_input {
+    int sp;
+    int ret;
+    int w;
+    int a;
+    int s;
+    int d;
+    int f;
+    int l;
+    int m;
+    int arr_u;
+    int arr_d;
+    int arr_l;
+    int arr_r;
+}               t_input;
 
-
+typedef struct  s_xy {
+    int x;
+    int y;
+}               t_xy;
 
 # include <libc.h>
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 15:37:45 by ldermign          #+#    #+#             */
-/*   Updated: 2021/04/01 14:14:28 by ldermign         ###   ########.fr       */
+/*   Updated: 2021/04/02 14:48:46 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,21 +48,16 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 void ft_circle(t_data img)
 {
 	int x = -800, y = -800, place = 400;
-	while (place > 0)
+	while (x < 800)
 	{
-		while (x < 800)
+		y = -800;
+		while (y < 800)
 		{
-			y = -800;
-			while (y < 800)
-			{
-				if (((x - place) * (x - place) + (y - place) * (y - place)) < 800)
-					my_mlx_pixel_put(&img, x, y, 0x0000FF00);
-				y += 2;
-			}
-			x += 2;
+			if (((x - place) * (x - place) + (y - place) * (y - place)) < 800)
+				my_mlx_pixel_put(&img, x, y, 0x0000FF00);
+			y += 2;
 		}
-		place -= 100;
-		x = -800;
+		x += 2;
 	}
 }
 
@@ -83,24 +78,79 @@ void ft_rainbow(t_data img)
 	ft_circle(img);
 }
 
-int		main()
+int		key_hook(int keycode, t_mlx *mlx)
 {
-	void *mlx;
-	void *win;
-	t_data img;
+	(void)mlx;
+	printf("key is [%d] in int.\n", keycode);
+	return (1);
+}
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 800, 800, "Cub3D");
-	img.img = mlx_new_image(mlx, 800, 800);
+int		mouse_hook(int keycode, t_mlx *temp)
+{
+	(void)temp;
+	printf("key is [%d] in int.\n", keycode);
+	return (1);
+}
+
+int		ft_close(int keycode, t_mlx *temp)
+{
+	(void)temp;
+	if (keycode == 53)
+		mlx_destroy_window(temp->mlx, temp->win);
+	return (1);
+}
+
+int		main(int ac, char **ag)
+{
+	// void *mlx;
+	// void *win;
+	t_data img;
+	(void)ac;
+	(void)ag;
+	// t_mlx	hook;
+	t_mlx	temp;
+
+	temp.mlx = mlx_init();
+	temp.win = mlx_new_window(temp.mlx, 800, 800, "Cub3D");
+	mlx_key_hook(temp.win, key_hook, &temp);
+	mlx_mouse_hook(temp.win, mouse_hook, &temp);
+	img.img = mlx_new_image(temp.mlx, 800, 800);
 	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
+	mlx_put_image_to_window(temp.mlx, temp.win, img.img, 0, 0);
 	ft_rainbow(img);
-	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_hook(temp.win, 2, 1L<<0, ft_close, &temp);
+	mlx_loop(temp.mlx);
 	return (0);
 }
 
-// RGB colors can be initialized as above, a few examples would be:
+/*
+RGB colors can be initialized as above, a few examples would be:
 
-// Red: 0x00FF0000;
-// Green: 0x0000FF00;
-// Blue: 0x000000FF;
+Red: 0x00FF0000;
+Green: 0x0000FF00;
+Blue: 0x000000FF;
+
+
+Keycode
+
+w = 0x0D
+space = 49
+arrow up = 126
+arrow down = 125
+arrow left = 123
+arrow right = 124
+s = 1
+a = 0
+d = 2
+f = 3
+m = 46
+l = 37
+enter = 36
+delete = 51
+left mouse = 1
+right mouse = 2
+molette haut = 5
+molette bas = 4
+esc = 53
+
+*/
