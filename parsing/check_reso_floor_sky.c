2788cb38cfc0377813b96be59e2059a388678891
ldermign <ldermign@student.42.fr>
2021-04-29 09:12:26 +0200

    normalement fin parsing avec norme

--- parsing/check_reso_floor_sky.c
+++ parsing/check_reso_floor_sky.c
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 10:03:46 by ldermign          #+#    #+#             */
-/*   Updated: 2021/04/27 15:27:13 by ldermign         ###   ########.fr       */
+/*   Updated: 2021/04/29 09:12:02 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,65 +38,55 @@ void	check_resolution(t_arg *data, char *str)
 		quit(data, "Wrong info in resolution.\n", 0, 0);
 }
 
+void	check_inside_split(t_arg *data, char **tmp)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = 0;
+	while (tmp[++i])
+	{
+		j = 0;
+		data->tmp = 0;
+		while (tmp[i][j])
+		{
+			if (ft_isdigit(tmp[i][j]))
+				data->tmp++;
+			if (tmp[i][j] != '\0' || tmp[i][j] == ' '
+				|| tmp[i][j] != 'F' || tmp[i][j] != 'C')
+				j++;
+		}
+		if (data->tmp == 0)
+			quit(data, "It's missing numbers in sky or floor.\n", 0, 0);
+	}
+	if (i != 3)
+		quit(data, "It's missing some info in sky or floor.\n", 0, 0);
+}
+
 void	check_floor_sky(t_arg *data, char *str)
 {
-	data->tmp = 2;
-	// while (*str && *str == ' ')
-	// 	str++;
-	// if (!ft_strncmp(str, "C ", 2) || !ft_strncmp(str, "F ", 2))
-	// 	str += 2;
-	// else
-	// 	quit(data, "Wrong info in sky or floor.\n", 0, 0);
-	// while (*str && (*str == ' ' || ft_isdigit(*str)))
-	// 	str++;
-	// while (*str && (*str == ' ' || *str == ','))
-	// {
-	// 	if (*str == ',')
-	// 		data->tmp++;
-	// 	str++;
-	// }
-	// while (*str && ft_isdigit(*str))
-	// 	str++;
-	// while (*str && (*str == ' ' || *str == ','))
-	// {
-	// 	if (*str == ',')
-	// 		data->tmp++;
-	// 	str++;
-	// }
-	// while (*str && ft_isdigit(*str))
-	// 	str++;
+	int		j;
+	char	**tmp;
 
-	int i;
-	int j;
-	char **tmp;
-
-	i = 0;
 	j = 0;
 	tmp = ft_split(str, ',');
 	data->tmp = 0;
 	while (tmp[0][j] == ' ')
 		j++;
-	if ((ft_strncmp(&tmp[0][j], "C ", 2) != 0) && (ft_strncmp(&tmp[0][j], "F ", 2) != 0))
+	if ((ft_strncmp(&tmp[0][j], "C ", 2) != 0)
+		&& (ft_strncmp(&tmp[0][j], "F ", 2) != 0))
 		quit(data, "Wrong info in sky or floor.\n", 0, 0);
-	while (tmp[i])
+	while (tmp[0][j])
 	{
-		j = 0;
-		while (tmp[i][j])
-		{
-			if (tmp[i][j] == ' ' || tmp[i][j] != 'F' || tmp[i][j] != 'C')
-				j++;
-			printf("[%s]\n", &tmp[i][j]);
-			if (ft_isdigit(tmp[i][j]))
-				data->tmp++;
-			j++;
-		}
-		printf("i = %d, j = %d.\n", i, j);
-		if (data->tmp == 0)
-			quit(data, "It's missing numbers in sky or floor.\n", 0, 0);
-		i++;
+		if ((tmp[0][j] == 'C' || tmp[0][j] == 'F')
+			|| (!ft_isdigit(tmp[0][j]) && tmp[0][j] != 'C' && tmp[0][j] != 'F'
+				&& tmp[0][j] != ' ' && tmp[0][j] != '\0'
+				&& tmp[0][j] != 'C' && tmp[0][j] != 'F'))
+			data->tmp++;
+		j++;
 	}
-	if (i != 3)
-		quit(data, "Wrong info in sky or floor.\n", 0, 0);
-	// if (tmp[i] < 3 || data->tmp != 2)// || *str != '\0' || !ft_isdigit(*(str - 1)))
-	// 	quit(data, "Wrong info in sky or floor.\n", 0, 0);
+	if (data->tmp > 1)
+		quit(data, "There is some wrong info in sky of floor.\n", 0, 0);
+	check_inside_split(data, tmp);
 }
