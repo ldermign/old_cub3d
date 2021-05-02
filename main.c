e9749987d510049a1e6f8ae2b1c462d061e136ad
ldermign <ldermign@student.42.fr>
2021-05-02 20:28:09 +0200

    test commit vscode

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 15:37:45 by ldermign          #+#    #+#             */
-/*   Updated: 2021/04/30 13:40:07 by ldermign         ###   ########.fr       */
+/*   Updated: 2021/05/02 20:28:09 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void	init_struct(t_arg *arg, t_spacemap *space)
 
 void	parsing(int ac, char **ag, t_arg *data)
 {
-
 	t_spacemap	space;
 
 	ft_check_arg(ac, ag);
@@ -29,13 +28,13 @@ void	parsing(int ac, char **ag, t_arg *data)
 	check_wrong_data_and_recup(data);
 	recup_map(data);
 	check_map(data, &space);
-	printf_struct_arg(*data, space);
+	// printf_struct_arg(*data, space);
 	// quit(&data, "All good.\n", 3, 0);
 }
 
-int		ft_close_escape(int keycode, t_arg *data)
+int		ft_close_escape(int keycode, t_mlx *img)
 {
-	(void)data;
+	(void)img;
 	if (keycode == 53)
 	{
 		printf("You pressed the escape button.\n");
@@ -46,9 +45,9 @@ int		ft_close_escape(int keycode, t_arg *data)
 	return (0);
 }
 
-int		ft_close_cross(t_arg *data)
+int		ft_close_cross(t_mlx *img)
 {
-	(void)data;
+	(void)img;
 	printf("You clicked on the cross.\n");
 	printf("Goodbye !\n");
 	// quit(data, "All good.\n", 3, 0);
@@ -56,9 +55,11 @@ int		ft_close_cross(t_arg *data)
 	return (0);
 }
 
-void	print_keycode(int keycode)
+int	print_keycode(int keycode, t_mlx *img)
 {
-	printf("Keycode is = %d'\n", keycode);
+	(void)img;
+	printf("Keycode is = %d.\n", keycode);
+	return (0);
 }
 
 void	my_mlx_pixel_put(t_mlx *data, int x, int y, int color)
@@ -69,36 +70,23 @@ void	my_mlx_pixel_put(t_mlx *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-//	place = endroit ou il sera place
-void ft_circle_pixel(t_mlx img, int x, int y, int place)
+int 	afficher_coor_souris(t_mlx *img)
 {
-	x = -400, y = -400, place = 400;
-	while (x < 800)
-	{
-		y = -400;
-		while (y < 800)
-		{
-			if (((x - place) * (x - place) + (y - place) * (y - place)) < 800)
-				my_mlx_pixel_put(&img, x, y, 0x0000FF00);
-			y += 2;
-		}
-		x += 2;
-	}
-}
-
-int		key_hook(int keycode, void *param)
-{
-	(void)param;
-	(void)keycode;
-	// printf("keycode is [%d].\n", keycode);
+	(void)img;
+	printf("x = %d, y = %d.\n", );
 	return (0);
 }
 
-int	test(t_mlx *img)
+int 	souris_entree()
 {
-	printf("img->x = %d, img->y = %d.\n", img->x, img->y);
-	my_mlx_pixel_put(img, img->x, img->y, 0x0000FF00);
-	mlx_put_image_to_window(img->mlx, img->win, img->img, 0, 0);
+	printf("Congrats ! You entered in the window...\n");
+	return (0);
+}
+
+int 	souris_sortie(t_mlx *img)
+{
+	(void)img;
+	printf("You are leaving so soon...\n");
 	return (0);
 }
 
@@ -128,23 +116,26 @@ int		main(int ac, char **ag)
 	img.mlx = mlx_init();
 	img.win = mlx_new_window(img.mlx, data.res_x, data.res_y, "Cub3D");
 
-	// mlx_key_hook(img.win, key_hook, print_keycode);
 	// mlx_mouse_hook(img.win, mouse_hook, &img);
 	
 	// img.img = mlx_xpm_file_to_image(img.mlx, data.east, &data.res_x, &data.res_y);
 	img.img = mlx_new_image(img.mlx, data.res_x, data.res_y);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.size_line, &img.endian);
 
-	// mlx_hook(img.win, 2, 1L<<0, move_pixel, &img);
+	// PAS TOUCHER
+	mlx_key_hook(img.win, print_keycode, &img);
 	mlx_hook(img.win, 2, 1L<<0, ft_close_escape, &img);
 	mlx_hook(img.win, 17, 1L<<0, ft_close_cross, &img);
 
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.size_line, &img.endian);
-	
-	// mlx_loop_hook(img.win, test, &img);
-	// mlx_loop(img.mlx);
 
-	// mlx_hook(img.win, 4, 1L<<0, move_pixel, &img);
-	mlx_key_hook(img.win, move_pixel, &img);
+
+	mlx_mouse_show();
+	// mlx_mouse_hook(img.win, afficher_coor_souris, &img);
+	// mlx_hook(img.win, 2, 1L<<0, move_pixel, &img);
+	mlx_hook(img.win, 7, 1L<<4, souris_entree, (void *)0);
+	mlx_hook(img.win, 8, 1L<<5, souris_sortie, (void *)0);
+	// mlx_hook(img.win, 6, 1L<<6, afficher_coor_souris, &img);
+	// mlx_key_hook(img.win, move_pixel, &img);
 
 	// my_mlx_pixel_put(&img, img.x, img.y, 0x0000FF00);
 	// mlx_put_image_to_window(img.mlx, img.win, img.img, 0, 0);
